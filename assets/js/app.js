96ebcbee9ffbeef99686c9b1f46c60182b1b3425
Raihan Kabir <raihancse46@gmail.com>
2022-03-15 12:16:21 +0600

    projects slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+    $('.projects_slide').owlCarousel({
+        autoplay: false,
+        loop: true,
+        margin: 0,
+        dots: false,
+        nav: true,
+        navText: ['<img src="assets/icons/arrow-left.svg" alt="arrow-left">', '<img src="assets/icons/arrow-right.svg" alt="arrow-left">'],
+        responsive: {
+            0: {
+                items: 1
+            }
+        }
+    })
+
+    //testmonial slide
     $('.testi_carousel').owlCarousel({
         autoplay: false,
         loop: false,
